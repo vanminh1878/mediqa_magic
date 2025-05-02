30f138ad3c80e1908b287552c72c4e8e2a43c43c
vanminh1878 <vanminh130804@gmail.com>
2025-05-02 17:02:07 +0700

    sửa setup

--- setup_kaggle.sh
+++ setup_kaggle.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 
-# Cài đặt các công cụ biên dịch cần thiết
-apt-get update
-apt-get install -y build-essential libomp-dev
-
 # Cài đặt các thư viện từ requirements.txt
 pip install -r requirements.txt
 
-# Cài đặt nmslib riêng để xử lý lỗi biên dịch
-pip install nmslib==2.1.1 --no-binary nmslib
-
-# Tải mô hình en_core_sci_sm từ scispacy
-pip install https://s3-us-west-2.amazonaws.com/ai2-s2-scispacy/releases/v0.5.1/en_core_sci_sm-0.5.1.tar.gz
 mkdir -p /kaggle/working/output/
